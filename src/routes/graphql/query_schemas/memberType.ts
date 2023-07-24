791aa08f5edf56ae144effd803d03b8955830bfd
Makar0nkin <ilya.makarenko2004@gmail.com>
2023-07-24 21:15:15 +0300

    fix: implement correct member type id

--- src/routes/graphql/query_schemas/memberType.ts
+++ src/routes/graphql/query_schemas/memberType.ts
@@ -1,11 +1,12 @@
 import { GraphQLList, GraphQLString } from "graphql";
 import { memberTypeType } from "../gql_types/memberType.js";
+import { memberTypeIdType } from "../gql_types/memberTypeId.js";
 
 export const memberTypeSchemaFields = {
   memberType: {
     type: memberTypeType,
     args: {
-      id: { type: GraphQLString },
+      id: { type: memberTypeIdType },
     },
   },
   memberTypes: {
--- src/routes/graphql/utils/types.ts
+++ src/routes/graphql/utils/types.ts
@@ -5,3 +5,8 @@ export type SchemaFieldsType = Record<string, iSchemaField>;
 export type iSubscribersOnAuthors = Required<
   Pick<iUserPrismaResponse, 'subscribedToUser' | 'userSubscribedTo'>
 >; 
+
+// export enum MemberTypeId {
+//   BASIC = 'basic',
+//   BUSINESS = 'business',
+// }
